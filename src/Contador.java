7aa046c2c17f5f889b164b4691cd1269135e59b3
ellenscosta <ellen_souzacosta22@hotmail.com>
2024-05-21 21:04:54 -0300

    classe Contador e exception personalizada criados

--- src/Contador.java
+++ src/Contador.java
@@ -1,9 +1,50 @@
+import java.util.Scanner;
 
 public class Contador {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int parametroUm;
+		int parametroDois;
+		
+		Scanner terminal = new Scanner(System.in);
+		
+		System.out.print("Digite o primeiro parâmetro: ");
+		parametroUm = terminal.nextInt();
+		
+		System.out.print("Digite o segundo parâmetro: ");
+		parametroDois = terminal.nextInt();
+		
+		try {
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException e) {
+			System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
+		}
+		
+		terminal.close();
+		
+	}
 
+	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+		
+		int contagem;
+		 
+		// o segundo parâmetro não poderá ser maior e nem igual ao primeiro para realizar a contagem
+		if (parametroDois <= parametroUm) {
+			throw new ParametrosInvalidosException();
+		}
+
+		contagem = parametroDois - parametroUm;
+		
+		System.out.println("\n" + "Contando do " + parametroUm + " ao " + parametroDois + ":");
+		
+		for (int x = 0; contagem >= x; x++) {
+			
+			System.out.println(parametroUm + x);
+			
+		}
+		
 	}
 
 }
---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+
+public class ParametrosInvalidosException extends Exception {
+	
+	private static final long serialVersionUID = -5355680207260699110L;
+
+}
